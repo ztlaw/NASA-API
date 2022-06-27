5d6c49bc7d4280cf942793b0ad33d7b6b4118126
Walt Lungan <waltlungan@gmail.com>
2022-06-26 21:43:41 -0700

    adding html and styles

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('hi there :)')
+
+document.querySelector('#button').addEventListener('click', getFetch)
+
+
+function getFetch(){
+const date = document.querySelector('#date').value
+//console.log(date)
+const url = `https://api.nasa.gov/planetary/apod?api_key=Xl2aWI9PpV43M6W9KL3vTFushHtGH3ogWxTGZmZR&date=${date}`
+//console.log(url)
+
+fetch(url)
+    .then( res => res.json())
+    .then( data => {
+        console.log(data);
+        document.querySelector('h1').innerHTML = data.title;
+    })
+}
